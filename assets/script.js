65fc86ed07c59aef79060291e7f14b3ba5b58b88
Nibbler <kwimbish@hotmail.com>
2022-03-17 14:53:28 -0500

    added aside and search var to js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var apiKey = "8a094c5efc564ac2b38192654221603";
+
+// form items
+var searchBtn = document.getElementById("searchBtn");
+var searchInput = document.getElementsByTagName("searchInput");
+
+// additional aside items
+var formEl = document.querySelector("#cityForm");
+var cityNameEl = document.getElementsByClassName("cityName");
+var currentDateEl = document.getElementsByClassName("currentDate");
+var seachHistoryEl = document.getElementsByClassName("pastCityList");
