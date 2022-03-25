7eb1456aa42f3a8dd1e4127224467e6accc0d966
Nibbler <kwimbish@hotmail.com>
2022-03-25 15:39:23 -0500

    correction to function uxIndex

--- assets/script.js
+++ assets/script.js
@@ -10,20 +10,20 @@ var searchBtn = document.getElementById("searchBtn");
 var searchInput = document.getElementsByClassName("searchInput");
 
 // additional aside/form items
-var cityName = document.getElementsByClassName("cityName");
+var cityNameEl = document.getElementById("cityName");
 var currentDate = document.getElementsByClassName("currentDate");
 var searchHistory = document.getElementsByClassName("pastCityList");
-var weatherIcon = document.getElementsByClassName("weatherIcon");
+var iconEl = document.getElementById("weatherIcon");
 
 // current city weather items/card items
-var temp = document.getElementsByClassName("temp");
-var humi = document.getElementsByClassName("humidity");
-var wind = document.getElementsByClassName("wind");
-var uv = document.getElementsByClassName("uv");
-var cardRow = document.getElementsByClassName("cardRow");
+var tempEl = document.getElementById("temp");
+var humiEl = document.getElementById("humidity");
+var windEl = document.getElementById("wind");
+var uvEl = document.getElementById("uv");
+var cardRow = document.getElementById("cardRow");
 
 // future forecast
-var futureForecast = document.querySelector("forecast");
+var futureForecast = document.querySelector(".cardRow");
 
 // to create the current date, borrowed from work-day-scheduler assignment
 var today = function() {
@@ -51,36 +51,36 @@ searchBtn.addEventListener("click", getWeather);
 function getWeather(searchedCity) {
   searchedCity.preventDefault()
   var city = document.getElementById("searchInput").value;
-  console.log(city);
+  // console.log(city);
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     .then(function(res) {
       return res.json();
     })
     .then(function(data) {
-      console.log(data);
+      // console.log(data);
       var cityToSave = {
         name: data.name,
         lat: data.coord.lat,
         lon: data.coord.lon,
       }
-      console.log(cityToSave);
+      // console.log(cityToSave);
       var historyToCheck = localStorage.getItem("searchHistory");
-      console.log(historyToCheck);
+      // console.log(historyToCheck);
       var historyCheck = JSON.parse(historyToCheck);
-      console.log(historyCheck);
+      // console.log(historyCheck);
       if (historyCheck === null) {
         var arrayToSave = [cityToSave];
         var arrayToString = JSON.stringify(arrayToSave);
-        console.log(arrayToString);
+        // console.log(arrayToString);
         localStorage.setItem('searchHistory', arrayToString);
-        console.log(localStorage.getItem('searchHistory'));
+        // console.log(localStorage.getItem('searchHistory'));
         renderBtns(arrayToSave);
       }else{
-        console.log('historyExists');
+        // console.log('historyExists');
         var searchedCity = JSON.parse(localStorage.getItem('searchHistory'));
         searchedCity.unshift(cityToSave);
         localStorage.setItem('searchHistory', JSON.stringify(searchedCity));
-        console.log(searchedCity);
+        // console.log(searchedCity);
         renderBtns(searchedCity);
         uvIndex(data.coord.lat, data.coord.lon);
       }
@@ -90,12 +90,12 @@ function getWeather(searchedCity) {
 // Creating city buttons in search history
 function renderBtns(cityArr) {
   $( "#pastCityList").empty();
-  console.log('functionToRenderBtns', cityArr);
+  // console.log('functionToRenderBtns', cityArr);
   for (let index = 0; index < cityArr.length; index++) {
     const element = cityArr[index];
-    console.log(element);
+    // console.log(element);
     var test = $(`<button>${element.name}</button>`).click(function () {
-      console.log('test');
+      // console.log('test');
     });
   $('#pastCityList').append(test);
   }
@@ -106,39 +106,51 @@ function renderBtns(cityArr) {
 }
 
 function uvIndex(lat, lon) {
-var fiveDayFore = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+var fiveDayFore = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&unit=standard&appid=${apiKey}`;
     fetch(fiveDayFore)
     .then(function(response) {
+      // console.log(response)
       return response.json();
     })
-    console.log()
     .then(function(data) {
-      futureForecast.innerHTML = ''
-      console.log(data);
-      var main = document.querySelector('.weatherInfo');
-      var index = document.createElement('div');
+      cardRow.innerHTML = ''
+      console.log("data: ", data);
+      // var main = document.querySelector('.weatherInfo');
+      // var index = document.createElement('div');
+      var temp = `Temp: ${data.current.temp}`;
+      var wind = `Wind: ${data.current.wind_speed}`;
+      var humidity = `Humidity: ${data.current.humidity}`;
       var uv =`UV Index: ${data.current.uvi}`;
-      index.innerHTML = uv;
-      main.appendChild(index);
+      var icon =` ${data.current.weather.icon}`;
+      tempEl.textContent = temp;
+      windEl.textContent = wind;
+      humiEl.textContent = humidity;
+      uvEl.textContent = uv;
+      // iconEl.href = icon;
+      console.log(tempEl, windEl, humiEl, uvEl)
+      // main.appendChild(index);
 
-      for (var i = 0; i < data.daily.length; i++)
-      if (i <= 5 && i > 0) {
-        var p = document.createElement('p');
-        var upcoming = `
-        <img src="http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png"/>
-        <p>
-        Temp: ${Math.round(data.daily[i].temp.day)} °F
-        </p>
-        <p>
-        Wind: ${data.daily[i].wind_speed} MPH
-        </p>
-        <p>
-        Humidity: ${data.daily[i].humidity}%
-        </p>
-        `;
-        p.innerHTML = upcoming;
-        futureForecast.appendChild(p);
-      }
+      // for (var i = 0; i < data.daily.length; i++) {
+      //   if (i <= 5 && i > 0) {
+      //     var p = document.createElement('p');
+      //     var upcoming = `
+      //     <img src="http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png"/>
+      //     <p>
+      //     Temp: ${Math.round(data.daily[i].temp.day)} °F
+      //     </p>
+      //     <p>
+      //     Wind: ${data.daily[i].wind_speed} MPH
+      //     </p>
+      //     <p>
+      //     Humidity: ${data.daily[i].humidity}%
+      //     </p>
+      //     `;
+      //     p.innerHTML = upcoming;
+      //     cardRow.appendChild(p);
+      //   }
+      // }
+    }).catch((error) => {
+      console.error(error);
     });
 }
 // grr. Why isn't this working?!?!
